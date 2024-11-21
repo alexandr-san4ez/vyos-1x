0e57d9880fddb0444207cbcf2c11a30e7af86db1
sarthurdev <965089+sarthurdev@users.noreply.github.com>
2024-11-21 22:05:49 +0100

    pki: T6809: Support system install of CA certificates
    (cherry picked from commit 0358f6c660e6130b94ca8c53ec7b34d796605dd8)

--- python/vyos/defaults.py
+++ python/vyos/defaults.py
@@ -34,6 +34,7 @@ directories = {
   'vyos_udev_dir' : '/run/udev/vyos',
   'isc_dhclient_dir' : '/run/dhclient',
   'dhcp6_client_dir' : '/run/dhcp6c',
+  'ca_certificates' : '/usr/local/share/ca-certificates/vyos',
 }
 
 systemd_services = {

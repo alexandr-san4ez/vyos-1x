14608b1df5bf29b5c6406d848922162fa8c8bb75
aapostoliuk <a.apostoliuk@vyos.io>
2025-05-12 17:24:03 +0300

    T7157: bgp: Added verification of the route-map existence in vrf import
     Added verification of the route-map existence in the vrf
    route-leaking.
    
    (cherry picked from commit e35315a46dd86919c54c2cdddf443fc88dfec8ae)

--- src/conf_mode/protocols_bgp.py
+++ src/conf_mode/protocols_bgp.py
@@ -576,6 +576,10 @@ def verify(bgp):
                         raise ConfigError(
                             'Please unconfigure import vrf commands before using vpn commands in dependent VRFs!')
 
+                # Verify if the route-map exists
+                if dict_search('route_map.vrf.import', afi_config) is not None:
+                    verify_route_map(afi_config['route_map']['vrf']['import'], bgp)
+
                 if (dict_search('route_map.vrf.import', afi_config) is not None
                         or  dict_search('import.vrf', afi_config) is not None):
                     # FRR error: please unconfigure vpn to vrf commands before
@@ -589,7 +593,6 @@ def verify(bgp):
                             or dict_search('route_map.vpn.export', afi_config) is not None) :
                         raise ConfigError('Please unconfigure route-map VPN to VRF commands before '\
                                           'using "import vrf" commands!')
-
 
                 # Verify that the export/import route-maps do exist
                 for export_import in ['export', 'import']:

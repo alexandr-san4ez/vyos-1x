a7fe70163b76606e4c40f82016b5341177dc300e
Christian Breunig <christian@breunig.cc>
2025-07-21 21:04:59 +0200

    vyos.utils: T7648: only provide one chown function
    Remove duplicate function definition vyos.utils.file.chown which was also
    provided by vyos.utils.permission.chown.
    
    (cherry picked from commit f6e25c242bd4623c452c7d034cd12aa47fee8953)

--- src/conf_mode/system_login.py
+++ src/conf_mode/system_login.py
@@ -30,7 +30,7 @@ from vyos.defaults import directories
 from vyos.template import render
 from vyos.template import is_ipv4
 from vyos.utils.dict import dict_search
-from vyos.utils.file import chown
+from vyos.utils.permission import chown
 from vyos.utils.process import cmd
 from vyos.utils.process import call
 from vyos.utils.process import rc_cmd

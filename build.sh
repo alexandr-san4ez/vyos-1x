be1c270f23908696699463de36a9485fdfaf04fe
John Estabrook <jestabro@vyos.io>
2025-09-02 12:26:39 -0500

    T7450: update commit hash for use of PCRE2 instead of PCRE

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#b8d221356a11e172bba4fc54ecb1a0f3133bf12a -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#8835a91e8383319a6bb98de9fd400b4eb7883eb5 -y'
 
 eval `opam config env`
 make clean

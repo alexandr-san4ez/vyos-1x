a8cc089a18c732d83db9e103de3199e8b2ab7404
Christian Breunig <christian@breunig.cc>
2025-09-22 20:49:15 +0200

    smoketest: T7858: make failfast main argument dynamic
    When smoketest debugging is enabled (by creating the file
    /tmp/vyos.smoketest.debug), all available smoketests will fail fast instead
    of running to completion. This helps reduce test time when something is
    broken or undergoing refactoring, as it avoids waiting for the full test suite
    to finish.
    
    (cherry picked from commit 2c521f1356378904e3d3a744960de68e8c5b62dc)

--- smoketest/scripts/cli/test_service_dhcp-server.py
+++ smoketest/scripts/cli/test_service_dhcp-server.py
@@ -920,4 +920,4 @@ class TestServiceDHCPServer(VyOSUnitTestSHIM.TestCase):
         self.assertTrue(host_json)
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=2, failfast=VyOSUnitTestSHIM.TestCase.debug_on())
--- smoketest/scripts/cli/test_system_login.py
+++ smoketest/scripts/cli/test_system_login.py
@@ -564,4 +564,4 @@ class TestSystemLogin(VyOSUnitTestSHIM.TestCase):
         self.op_mode(['reboot', 'cancel'])
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=2, failfast=VyOSUnitTestSHIM.TestCase.debug_on())

e8c8dd090c7004deae362a645120892eceadc01b
Christian Breunig <christian@breunig.cc>
2025-10-29 11:06:41 +0100

    pki: T7953: systemd certbot.service must not use PrivateTmp
    PrivateTmp needs to be disabled, otherwise we can not load the current
    VyOS configuration to reconfigure the service automatically when certbot
    runs.
    
    (cherry picked from commit cfe00ff81dc09ea0a45e2ec3498a33a1d331d15a)

--- src/helpers/vyos-certbot-renew-pki.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/vbash
-# Please note that this file is required in order to properly setup a VyOS
-# ConfigSession for the CLI script to work on the system. Directly calling
-# the CLI helper script from e.g. systemd certbot.timer will not work, as there
-# will be no Config() available to the CLI script
-source /opt/vyatta/etc/functions/script-template
-/usr/libexec/vyos/op_mode/pki.py --action certbot_renew

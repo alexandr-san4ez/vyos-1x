0592530c45898adb2ce031d87a4efc5c3efde91b
Christian Breunig <christian@breunig.cc>
2025-11-02 09:30:05 +0100

    container: T7305: fix VRF loss when restarting pods
    Container networks are only started when there is at least one active consumer.
    If a network is created without any attached containers, it does not need to be
    assigned to a VRF yet.
    
    When the last container in a pod is stopped, its associated container network
    is removed. Upon container restart, the kernel recreates the network, but the
    VRF assignment may be lost in the process.
    
    This change ensures that all container networks are correctly reattached to
    their designated VRFs when a pod restarts.
    
    (cherry picked from commit ae6d3437dc3247c396444b7f65eb49a98b0dd800)

--- src/op_mode/container.py
+++ src/op_mode/container.py
@@ -127,11 +127,21 @@ def show_network(raw: bool):
 
 def restart(name: str):
     from vyos.utils.process import rc_cmd
+    from vyos.config import Config
+    from vyos.container import restart_network
 
     rc, output = rc_cmd(f'systemctl restart vyos-container-{name}.service')
     if rc != 0:
         print(output)
         return None
+
+    if rc == 0:
+        conf = Config()
+        container = conf.get_config_dict(['container'], key_mangling=('-', '_'),
+                                    no_tag_node_value_mangle=True,
+                                    get_first_key=True,
+                                    with_recursive_defaults=True)
+        restart_network(container)
     print(f'Container "{name}" restarted!')
     return output
 

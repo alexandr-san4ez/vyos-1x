78fc9924ab8bfe2d2528091fdc7e761899169fda
Christian Breunig <christian@breunig.cc>
2025-12-19 21:10:14 +0100

    bond: T8084: remove dynamic interface type detection via vyos.ifconfig.Section
    Aggregated / bonded interfaces do only work with ethernet interfaces as
    underlaying link. There is no need to "dynamically" detect that eth0, eth1 or
    any other interface starting with eth is an ethernet interface.
    
    Remove calls to vyos.ifconfig.Section().
    
    (cherry picked from commit a7af7360e3f2d4635d7bf0340d183f01e3846aa9)

--- src/conf_mode/interfaces_bonding.py
+++ src/conf_mode/interfaces_bonding.py
@@ -31,7 +31,6 @@ from vyos.configverify import verify_vlan_config
 from vyos.configverify import verify_vrf
 from vyos.ifconfig import BondIf
 from vyos.ifconfig.ethernet import EthernetIf
-from vyos.ifconfig import Section
 from vyos.template import render_to_string
 from vyos.utils.assertion import assert_mac
 from vyos.utils.dict import dict_search
@@ -108,8 +107,7 @@ def get_config(config=None):
             # ethernet commit again in apply function
             # to apply options under ethernet section
             set_dependents('ethernet', conf, interface)
-            section = Section.section(interface) # this will be 'ethernet' for 'eth0'
-            if conf.exists([section, interface, 'disable']):
+            if conf.exists(['ethernet', interface, 'disable']):
                 tmp[interface] = {'disable': ''}
             else:
                 tmp[interface] = {}
@@ -138,14 +136,8 @@ def get_config(config=None):
                 bond['shutdown_required'] = {}
                 bond['member']['interface'][interface].update({'new_added' : {}})
 
-            # Check if member interface is disabled
-            conf.set_level(['interfaces'])
-
-            section = Section.section(interface) # this will be 'ethernet' for 'eth0'
-            if conf.exists([section, interface, 'disable']):
-                if tmp: bond['member']['interface'][interface].update({'disable': ''})
-
-            conf.set_level(old_level)
+            if 'disable' in interface_ethernet_config:
+                bond['member']['interface'][interface].update({'disable': ''})
 
             # Check if member interface is already member of another bridge
             tmp = is_member(conf, interface, 'bridge')
